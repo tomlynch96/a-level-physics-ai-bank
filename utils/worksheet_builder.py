e449004e5c003df1ebd2000defc59f61cfc64fbe
tomlynch96 <tomlynch96@gmail.com>
2025-06-19 07:30:05 +0100

    Update worksheet_builder.py

--- utils/worksheet_builder.py
+++ utils/worksheet_builder.py
@@ -1,1 +1,18 @@
+import random
+from copy import deepcopy
+
+def build_question_instance(q):
+    """Return a copy of the question with values filled in (if any)."""
+    instance = deepcopy(q)
+    instance["question_text"] = instance["question_template"]
+    return instance
+
+def get_linked_mcq(q):
+    """Return the independently authored MCQ testing the same idea."""
+    return {
+        "text": q['mcq']['question'],
+        "options": q['mcq']['options'],
+        "correct": q['mcq']['correct'],
+        "explanation": q['mcq'].get('explanation', '')
+    }
 
